59fe9679c682900bee66513c78e3d2fff1bab746
eliyaser <eliyaser3121@gmail.com>
2022-01-24 18:00:08 +0530

    Today update 24_01_2022

---
+++ 03-Developer-Skills/skilltest.js
@@ -0,0 +1,57 @@
+"use strict";
+
+// console.log(`i am from skilltest`);
+// Qustion number 1 ***********
+// const celab = ["eliyaser", "rajkumar", "sivanasan", "sathiyaraj"];
+
+// console.log(celab[1][3]);
+
+// Qustion number 2***********
+
+// const users = Promise.resolve(["u1", "u2", "u3"]);
+
+// function addAdmin(users) {
+//   users.push("ADMIN");
+// }
+// addAdmin();
+// users
+//   .then((users) => {
+//     addAdmin(users);
+//   })
+//   .then((users) => {
+//     console.log(users);
+//   });
+
+// Qustion number 3***********
+
+// const OFF_TIMES = [
+//   "1X PER WEEK",
+//   "AS OFTEN AS NEEDED",
+//   "ONLY FOR BATHROOM BREAKS",
+//   "3X PER WEEK",
+//   "NEVER",
+// ];
+
+// for (let i = 0; i < OFF_TIMES.length - 1; i++) {
+//   console.log(OFF_TIMES[i]);
+// }
+//OUT PUT only print 4 arrays
+
+// Qustion number 4 ***********
+// const alvin = { name: "Alvin" };
+// const theodore = { name: "theodore" };
+// const simon = { name: "simon" };
+
+// alvin.next = theodore;
+// theodore.next = simon;
+// simon.next = alvin;
+
+// let hotpotato = theodore;
+
+// for (let i = 0; i < 4; i++) {
+//   hotpotato = hotpotato.next;
+// }
+
+// console.log(`${hotpotato.name} is the hot potato`);
+
+// OUTPUT simon
